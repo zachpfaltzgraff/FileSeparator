906604d2c93f9fc9801d345d65f3bfc417e15111
zachpfaltzgraff <141784641+zachpfaltzgraff@users.noreply.github.com>
2024-01-31 10:56:39 -0500

    FileExtensions
    got the fileExtensions working so it saves the value

--- src/main/java/com/example/fileseparator/HelloApplication.java
+++ src/main/java/com/example/fileseparator/HelloApplication.java
@@ -1,6 +1,7 @@
 package com.example.fileseparator;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -12,12 +13,11 @@ import javafx.stage.Stage;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 
 public class HelloApplication extends Application {
     private String selectedFilePath;
-    private int i = 2;
-    private String[] Extension = new String[5];
+    private int i = 0;
+    private String[] fileExtensions = new String[5];
     @Override
     public void start(Stage stage) throws IOException {
         BorderPane borderPane = new BorderPane();
@@ -37,14 +37,14 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
         stage.show();
         stage.setMaxHeight(400);
-        stage.setMaxWidth(400);
+        stage.setMaxWidth(500);
     }
 
     private Button getStartingFolder(Stage stage, GridPane gridPane) {
         DirectoryChooser directoryChooser = new DirectoryChooser();
         Button chooseFileBtn = new Button();
         chooseFileBtn.setText("Choose Starting Folder");
-        chooseFileBtn.setMinWidth(250);
+        chooseFileBtn.setMinWidth(225);
         chooseFileBtn.setOnAction(e -> {
             File selectedFile = directoryChooser.showDialog(stage);
             if (selectedFile != null) {
@@ -52,8 +52,8 @@ public class HelloApplication extends Application {
                 System.out.println("Selected File Path: " + selectedFilePath);
                 chooseFileBtn.setDisable(true);
 
-                Label label = new Label("File Was Selected");
-                label.setMinWidth(250);
+                Label label = new Label("Folder Was Selected");
+                label.setMinWidth(110);
                 gridPane.setMargin(label, new Insets(15, 0, 0, 0));
                 gridPane.add(label, 1, 0);
 
@@ -65,31 +65,49 @@ public class HelloApplication extends Application {
 
     private void selectFileExtensions(Stage stage, GridPane gridPane) {
         Button addExtensionBtn = new Button();
-        addExtensionBtn.setMinWidth(250);
+        addExtensionBtn.setMinWidth(225);
         addExtensionBtn.setText("Add File Extension (Max 5)");
         gridPane.setMargin(addExtensionBtn, new Insets(0, 0, 0, 15));
         addExtensionBtn.setOnAction(e -> {
-
             if (!addExtensionBtn.isDisable()) {
                 Label label = new Label("Write Extension in the format \"extension\":");
                 gridPane.setMargin(label, new Insets(0, 0, 0, 15));
-                label.setMinWidth(250);
-                TextField textField = new TextField();
-                textField.setMaxWidth(100);
+                label.setMinWidth(225);
 
-                gridPane.add(label, 0, i);
-                gridPane.add(textField, 1, i);
+                TextField textField = new TextField();
+                textField.setPromptText("jpeg");
+                textField.setMaxWidth(110);
+                gridPane.setMargin(textField, new Insets(0, 0, 0, 15));
+
+                Button submitBtn = new Button();
+                submitBtn.setText("Submit");
+                submitBtn.setMinWidth(110);
+                gridPane.setMargin(submitBtn, new Insets(0, 0, 0, 15));
+                submitBtn.setOnAction(event -> {
+                    submit(event, textField, i);
+
+                    submitBtn.setDisable(true);
+                });
+
+
+                gridPane.add(label, 0, i + 2);
+                gridPane.add(textField, 1, i + 2);
+                gridPane.add(submitBtn, 2, i + 2);
 
                 i++;
-                
-                if (i >= 7) {
+                if (i >= 5) {
                     addExtensionBtn.setDisable(true);
                 }
             }
         });
         gridPane.add(addExtensionBtn, 0, 1);
 
+    }
 
+    private void submit(ActionEvent event, TextField textField, int i) {
+        fileExtensions[i] = textField.getText();
+
+        System.out.println(fileExtensions[i]);
     }
 
     public static void main(String[] args) {
